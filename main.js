a0e5b6a38d924e2fcb61760dbb6009db18dff2e1
hamjaehyeong <ham6729@gmail.com>
2023-05-21 11:01:43 +0900

    사운드 추가

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-
+let gameOverSound = document.getElementById('gameOverSound');
 //canvas setting
 let gameOn = true;
 
-let score=0;
+let score = 0;
 
 let canvas;
 let ctx;
 canvas = document.createElement("canvas")
 ctx = canvas.getContext("2d")
 
-canvas.width=400;
-canvas.height=700;
+canvas.width = 400;
+canvas.height = 700;
 document.body.appendChild(canvas);
 
 ////////////////////////////
@@ -19,29 +19,60 @@ let bgY2 = -canvas.height;
 let bgSpeed = 1;
 /////////////////////////////
 
-let backgroundImage, spaceshipImage,bulletImage,enemyImage,gameOverImage;
+let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
 
 //우주선 좌표
-let spaceshipX = canvas.width/2-32;
-let spaceshipY = canvas.height-64;
+let spaceshipX = canvas.width / 2 - 32;
+let spaceshipY = canvas.height - 64;
 
 
 function loadImage() {
     backgroundImage = new Image();
-    backgroundImage.src="image/background.jpg";
+    backgroundImage.src = "image/background.jpg";
 
     spaceshipImage = new Image();
-    spaceshipImage.src="image/player.png";
+    spaceshipImage.src = "image/player.png";
 
     bulletImage = new Image();
-    bulletImage.src="image/bullet.png";
+    bulletImage.src = "image/bullet.png";
 
     enemyImage = new Image();
     enemyImage.src = "image/enemy-big.png";
 
     gameOverImage = new Image();
-    gameOverImage.src ="image/gameover.png"
+    gameOverImage.src = "image/gameover.png"
 }
+
+
+function playGameOverSound() {
+    gameOverSound.play();
+}
+function playBulletSound() {
+    let createBulletSound = document.createElement('audio')
+    createBulletSound.src = "sound/bulletSound.mp3"
+    createBulletSound.preload = "auto"
+    document.body.append(createBulletSound)
+    createBulletSound.play();
+
+    // 1.2초 후에 오디오 요소를 삭제합니다.
+    setTimeout(function () {
+        createBulletSound.remove();
+    }, 1200);
+}
+
+function playExplosionSound() {
+    let createExplosionSound = document.createElement('audio')
+    createExplosionSound.src = "sound/explosionSound.mp3"
+    createExplosionSound.preload = "auto"
+    document.body.append(createExplosionSound)
+    createExplosionSound.play();
+
+    // 1.2초 후에 오디오 요소를 삭제합니다.
+    setTimeout(function () {
+        createExplosionSound.remove();
+    }, 1200);
+}
+
 
 let keysDown = {}
 
@@ -66,35 +97,36 @@ function Bullet() {
                 score++;
                 this.alive = false;
                 enemyList.splice(i, 1)
+                playExplosionSound()
                 break;
             }
         }
     }
     this.checkOut = function () {
-       if(this.y<0){
-        this.alive=false;
+        if (this.y < 0) {
+            this.alive = false;
 
-       } 
+        }
     }
 }
 function generateRandomValue(min, max) {
-    let randomNum = Math.floor(Math.random()*(max-min+1))+min
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
     return randomNum
 }
 
 let enemyList = []// 적(big)을 저장하는 리스트
 function Enemy() {
-    this.x=0;
-    this.y=0;
-    this.init=function () {
-        this.x = generateRandomValue(0, canvas.width-32)
+    this.x = 0;
+    this.y = 0;
+    this.init = function () {
+        this.x = generateRandomValue(0, canvas.width - 40)
         enemyList.push(this)
     }
     this.update = function () {
         this.y += 2; //enemy 속도 조절
-        
-        if(this.y>=canvas.height -32){
-            gameOn =false;
+
+        if (this.y >= canvas.height - 32) {
+            gameOn = false;
             console.log("오버")
         }
     }
@@ -103,8 +135,7 @@ function Enemy() {
 function createEnemy() {
     let e = new Enemy;
     e.init();
-    console.log("적생성");
-}   
+}
 
 
 
@@ -124,25 +155,25 @@ function createEnemyWrapper() {
 ///////
 
 
-function createBullet(){
+function createBullet() {
     let b = new Bullet;    //여기서 궁금한점: 여러개를 만들면 b가 중복되는거 아님?
     b.init();
 
-    console.log("총알생성");
-    console.log("새로운총알리스트",bulletList);
+    console.log("새로운총알리스트", bulletList);
 
 }
 
-function setupKeyboardListener(){
-    document.addEventListener("keydown", function(event){
+function setupKeyboardListener() {
+    document.addEventListener("keydown", function (event) {
         keysDown[event.key] = true;
-        console.log('값은?', keysDown);
+
     })
-    document.addEventListener("keyup", function(event){
+    document.addEventListener("keyup", function (event) {
         delete keysDown[event.key]
 
-        if(event.key===" "){
-            createBullet() //총알 생성
+        if (event.key === " " && gameOn === true) {
+            createBullet(); //총알 생성
+            playBulletSound();
         }
 
     })
@@ -150,38 +181,38 @@ function setupKeyboardListener(){
 
 
 function update() {
-    if( 'ArrowRight' in keysDown){
-        spaceshipX+=5;
+    if ('ArrowRight' in keysDown) {
+        spaceshipX += 5;
     }
-    if( 'ArrowLeft' in keysDown){
-        spaceshipX-=5;
+    if ('ArrowLeft' in keysDown) {
+        spaceshipX -= 5;
     }
-    
-    if(spaceshipX<=0){spaceshipX=0;}
-    if(spaceshipX>=352){spaceshipX=352;}
+
+    if (spaceshipX <= 0) { spaceshipX = 0; }
+    if (spaceshipX >= 352) { spaceshipX = 352; }
 
 
 
     for (let i = 0; i < bulletList.length; i++) {
-        if(bulletList[i].alive){
-        bulletList[i].update()
-        bulletList[i].checkHit()
-        bulletList[i].checkOut()
-        if(!bulletList[i].alive){
-            bulletList.splice(i,1)
+        if (bulletList[i].alive) {
+            bulletList[i].update()
+            bulletList[i].checkHit()
+            bulletList[i].checkOut()
+            if (!bulletList[i].alive) {
+                bulletList.splice(i, 1)
+            }
+
         }
 
     }
-  
-    }
-    
+
     for (let i = 0; i < enemyList.length; i++) {
         enemyList[i].update()
     }
 
 }
 
-function render(){
+function render() {
     ///
     ctx.drawImage(backgroundImage, 0, bgY1, canvas.width, canvas.height);
     ctx.drawImage(backgroundImage, 0, bgY2, canvas.width, canvas.height);
@@ -200,15 +231,15 @@ function render(){
 
 
     ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY)
-    ctx.fillText(`Score:${score}`,20, 30)
+    ctx.fillText(`Score:${score}`, 20, 30)
     ctx.fillStyle = 'white'
-    ctx.font ="20px Arial"
+    ctx.font = "20px Arial"
 
     for (let i = 0; i < bulletList.length; i++) {
-        if(bulletList[i].alive){
+        if (bulletList[i].alive) {
             ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y)
         }
-        
+
     }
     for (let i = 0; i < enemyList.length; i++) {
         ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y)
@@ -217,13 +248,15 @@ function render(){
 }
 
 function main() {
-   if(gameOn){
-    update();
-    render();
-    requestAnimationFrame(main);} 
-    else{
-        ctx.drawImage(gameOverImage, 50, 150, 300, 300 );
+    if (gameOn) {
+        update();
+        render();
+        requestAnimationFrame(main);
+    }
+    else {
+        ctx.drawImage(gameOverImage, 50, 150, 300, 300);
         clearTimeout(timeoutId);
+        playGameOverSound();
     }
 }
 
